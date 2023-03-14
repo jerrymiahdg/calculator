90416d61e1bb3116ec034097bc450748c2af2bcc
jerrymiahdg <124826126+jerrymiahdg@users.noreply.github.com>
2023-03-14 07:40:02 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,12 +1,15 @@
 const numBtn = []
 const rows = []
-for(let i = 0; i < 10; i++) {
-    numBtn.push(document.querySelector(`#num-btn-${i}`))
-    numBtn[i].addEventListener('click', numClick())
-}
+const grid = document.querySelector('.grid')
 function numClick() {
     return function() {
         rows.push(document.createElement('p'))
-        rows[i]
+        rows[i].className = 'row'
+        rows[i].textContent = i
+        grid.appendChild(rows[i])
     }
+}
+for(let i = 0; i < 10; i++) {
+    numBtn.push(document.querySelector(`#num-btn-${i}`))
+    numBtn[i].addEventListener('click', numClick)
 }
